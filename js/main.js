39c7e38497c54a2b54ebf2b0bae3d75a33ae3d53
LIVENVARANE <ulucqs@gmail.com>
2021-12-22 13:45:02 +0100

    header more buttons system

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ function link(where, newpage) {
         window.open(where, '_blank');
     }
 }
+
+function toggleHeaderMore() {
+    if(document.querySelector('.more-container').classList.contains('shown')) { //normal buttons mode
+        document.querySelector('.media-container').classList.remove('shown');
+        document.querySelector('.more-container').classList.remove('shown');
+        document.querySelector('.buttons-container').classList.add('shown');
+    } else {
+        document.querySelector('.media-container').classList.add('shown');
+        document.querySelector('.more-container').classList.add('shown');
+        document.querySelector('.buttons-container').classList.remove('shown');
+    }
+}
